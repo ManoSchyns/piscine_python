b90b694eda99b7f1d2de4bae17f0705b8d58c9e3
ManoSchyns <mano@schyns.net>
2026-05-13 08:27:30 +0200

    on vance

---
+++ data_quest/ex0/ft_command_quest.py
@@ -0,0 +1,17 @@
+import sys
+
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    args: list[str] = sys.argv
+    len: int = len(args)
+    i: int = 1
+
+    print("Program name: " + args[0])
+    if(len == 1):
+        print("No arguments provided!")
+    else:
+        print("Arguments received:", len-1)
+        while(i < len):
+            print("Argument ", i, ": " + args[i], sep="")
+            i += 1
+    print("Total arguments:", len)
